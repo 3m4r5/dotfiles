5abcc4e0001039257eb4907badaf867decc3a3c5
Omar Al-Nadi <om4r37@gmail.com>
2024-10-01 21:59:58 +0300

    fix installation

--- install.sh
+++ install.sh
@@ -5,5 +5,8 @@ dnf -y copr enable solopasha/hyprland
 # list installed packages: dnf repoquery --userinstalled
 #                 for vscode  V                 for junction and Zen V            for yazi V             V for btop
 dnf install -y hyprland git micro btop polkit-gnome rofimoji flatpak flameshot ffmpegthumbnailer rocm-smi waybar kitty network-manager-applet blueman rofi-wayland hyprland-autoname-workspaces libglvnd-gles pavucontrol unzip python3 python3-requests mpv firewall-applet imv nwg-clipman
+# flathub:
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+flatpak -y install flathub io.github.zen_browser.zen flathub re.sonny.Junction
 sed -i -e 's/Noto Color/Apple Color Emoji<\/family><family>Noto Color/g' /etc/fonts/conf.d/60-generic.conf # TODO 45
 rm -f install.sh
