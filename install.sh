cb79198819fb59b5b41fb9f22e4d082c2003f052
3m4r5 <63432932+3m4r5@users.noreply.github.com>
2024-11-25 17:57:19 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ timedatectl set-timezone Asia/Amman # fix timezone
 dnf -y install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm # rpm fusion (for ffmpegthumbnailer)
 dnf -y copr enable solopasha/hyprland
 #                                             for junction and Zen V            for yazi V             V for btop (amd)
-dnf install -y hyprland git micro btop hyprpolkitagent rofimoji flatpak flameshot ffmpegthumbnailer rocm-smi waybar kitty network-manager-applet blueman rofi-wayland hyprland-autoname-workspaces libglvnd-gles pavucontrol unzip python3 python3-requests mpv firewall-applet imv nwg-clipman zathura
+dnf install -y hyprland git micro btop hyprpolkitagent rofimoji flatpak flameshot ffmpegthumbnailer rocm-smi waybar kitty network-manager-applet blueman rofi-wayland hyprland-autoname-workspaces libglvnd-gles pavucontrol unzip python3 python3-requests mpv firewall-applet imv nwg-clipman zathura google-noto-sans-arabic-fonts
 # flathub:
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 flatpak -y install flathub io.github.zen_browser.zen flathub re.sonny.Junction
