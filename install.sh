8fa557ed93469cd392a331af077292320ab45c55
3m4r5 <63432932+3m4r5@users.noreply.github.com>
2025-08-09 21:59:49 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,37 +2,37 @@ timedatectl set-timezone Asia/Amman # fix timezone
 dnf -y install "https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm" "https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm" # rpm fusion (for ffmpegthumbnailer)
 dnf -y copr enable solopasha/hyprland
 dnf install -y\
-hyprland\
-git\
-micro\
-btop\
-hyprpolkitagent\
-rofimoji\
-flatpak\
-flameshot\
-ffmpegthumbnailer\
-rocm-smi\
-waybar\
-kitty\
-network-manager-applet\
-blueman\
-rofi-wayland\
-hyprland-autoname-workspaces\
-libglvnd-gles\
-pavucontrol\
-unzip\
-python3\
-python3-requests\
-mpv\
-imv\
-nwg-clipman\
-zathura\
-SwayNotificationCenter\
-power-profiles-daemon\
-PackageKit-command-not-found\
-lm_sensors\
-adwaita-gtk2-theme\
-hyprland-devel
+ hyprland\
+ git\
+ micro\
+ btop\
+ hyprpolkitagent\
+ rofimoji\
+ flatpak\
+ flameshot\
+ ffmpegthumbnailer\
+ rocm-smi\
+ waybar\
+ kitty\
+ network-manager-applet\
+ blueman\
+ rofi-wayland\
+ hyprland-autoname-workspaces\
+ libglvnd-gles\
+ pavucontrol\
+ unzip\
+ python3\
+ python3-requests\
+ mpv\
+ imv\
+ nwg-clipman\
+ zathura\
+ SwayNotificationCenter\
+ power-profiles-daemon\
+ PackageKit-command-not-found\
+ lm_sensors\
+ adwaita-gtk2-theme\
+ hyprland-devel
 
 # flathub:
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
